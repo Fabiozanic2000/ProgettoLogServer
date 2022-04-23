36c8505b81ce30b6d7bd92a01044ed248af82c74
superflem <alex.flem@hotmail.it>
2022-04-23 20:06:17 +0200

    modificata la mappa

--- code/frontend/src/js/Mappa.js
+++ code/frontend/src/js/Mappa.js
@@ -2,28 +2,9 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
 import '../css/Mappa.css';
 
-//liberire per fare l'icona diversa
-import * as L from "leaflet";
-import { useState } from "react";
-
 const Mappa = () => {
 
-    // questo codice serve per dare un'icona diversa al marker
-    const LeafIcon = L.Icon.extend({
-        options: {}
-      });
     
-      const blueIcon = new LeafIcon({
-          iconUrl:
-            "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|abcdef&chf=a,s,ee00FFFF"
-        }),
-        greenIcon = new LeafIcon({
-          iconUrl:
-            "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|2ecc71&chf=a,s,ee00FFFF"
-        });
-    
-      const [icona, setIcon] = useState(blueIcon);
-
 
 
 
@@ -43,7 +24,7 @@ const Mappa = () => {
                     </Popup>
                 </Marker>
 
-                <Marker position={[60.505, 55.09] /* puntatore russia */} icon={icona}>
+                <Marker position={[60.505, 55.09] /* puntatore russia */}>
                     <Popup>
                         Russia
                     </Popup>
