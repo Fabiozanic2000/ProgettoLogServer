7bd1ab19993a5e2ab6d317fd1ff8ecd08b74d868
Francesco Malferrari <malfigo48@gmail.com>
2022-05-03 23:25:38 +0200

    Migliorata qualità del codice

--- code/backend/src/server/Login.java
+++ code/backend/src/server/Login.java
@@ -14,34 +14,33 @@ import java.util.Scanner;
 import org.json.JSONObject;
 import java.sql.SQLException;
 
+/**
+ * Classe che implementa il login
+ */
+
 public class Login implements HttpHandler {
 
     private UtentiDb db;
-    private String email;
-    private String password;
-    private int rCode;
-    private String response;
 
     /**
      * Costruttore, riceve il db in input
-     * @param db
+     * @param db database degli utenti
      */
-    public Login (UtentiDb db)
-    {
+    public Login (UtentiDb db) {
         this.db = db;
-        email = "";
-        password = "";
-        response = "";
-        rCode = 0;
     }
 
     /**
-     * Metodo che viene invocato quando si contatta /login
-     * @param t
+     * Metodo che viene invocato quando si contatta /login .
+     * @param t rappresenta la connessione HTTP tra client e server
      * @throws IOException
      */
     @Override
     public void handle(HttpExchange t) throws IOException {
+        String email = "";
+        String password = "";
+        String response = "";
+        int rCode = 0;
         URI requestedUri = t.getRequestURI(); //prende l'uri contattato
         try
         {
@@ -67,7 +66,8 @@ public class Login implements HttpHandler {
                 id = db.login(email, password);
 
                 if (id != -1) { // se il login è andato a buon fine setto il cookie e restituisco l'id
-                    t.getResponseHeaders().set("Set-Cookie", "id=" + id + "; HttpOnly; Expires=900");
+                    t.getResponseHeaders()
+                            .set("Set-Cookie", "id=" + id + "; HttpOnly; Expires=900");
                     response = "{\"id\": \"pippo\"}";
                     rCode = 200;
 
@@ -76,12 +76,10 @@ public class Login implements HttpHandler {
                     rCode = 200;
                 }
             }
-            else if ("OPTIONS".equals(t.getRequestMethod()) && requestedUri.compareTo(new URI("/login"))==0) // per il preflight
-            {
+            else if ("OPTIONS".equals(t.getRequestMethod()) && requestedUri.compareTo(new URI("/login"))==0) {  // per il preflight
                 rCode = 200;
             }
-            else //diverso da post e options oppure usa un'altro url
-            {
+            else { //diverso da post e options oppure usa un'altro url
                 System.out.println("URI non trovato");
                 rCode = 404;
                 response = "{\"errore\": \"Pagina non trovata\"}";
@@ -93,19 +91,23 @@ public class Login implements HttpHandler {
         } catch (SQLException e) { //errore db
             throw new RuntimeException(e);
         }
-        catch (Exception e) //errore nella lettura del body della request
-        {
+        catch (Exception e) { //errore nella lettura del body della request
             e.printStackTrace();
         }
 
         //invio la risposta al client (gli header servono per le politiche di cors)
         String origine = t.getRequestHeaders().get("Origin").toString(); // l'origine serve per l'header sotto
         origine = origine.substring(1, origine.length()-1);
-        t.getResponseHeaders().add("Access-Control-Allow-Origin", origine);
-        t.getResponseHeaders().add("Access-Control-Allow-Headers","origin, content-type, accept, authorization");
-        t.getResponseHeaders().add("Access-Control-Allow-Credentials", "true");
-        t.getResponseHeaders().add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-        t.getResponseHeaders().add("Content-Type", "application/json"); //dico che la risposta sarà un json un json
+        t.getResponseHeaders()
+                .add("Access-Control-Allow-Origin", origine);
+        t.getResponseHeaders()
+                .add("Access-Control-Allow-Headers","origin, content-type, accept, authorization");
+        t.getResponseHeaders()
+                .add("Access-Control-Allow-Credentials", "true");
+        t.getResponseHeaders()
+                .add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        t.getResponseHeaders()
+                .add("Content-Type", "application/json"); //dico che la risposta sarà un json un json
 
         t.sendResponseHeaders(rCode, response.length());
         OutputStream os = t.getResponseBody(); //chiude la comunicazione
