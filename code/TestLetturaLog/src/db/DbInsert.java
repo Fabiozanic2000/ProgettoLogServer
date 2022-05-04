6dc80f54b9eb121648ab77aca1ab1bdff0b10d57
Il-castor <castorfrancesco@gmail.com>
2022-05-04 14:46:04 +0200

    Aggiunto parser semifunzionante da finire

---
+++ code/TestLetturaLog/src/db/DbInsert.java
@@ -0,0 +1,43 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DbInsert {
+    public boolean insert(String request, String auth, String ident, String httpmethod, String time, int response,
+                          int bytes, String clientip, String rawrequest, String data, String timestamp, String paese,
+                          Dblog db) {
+        Connection c = db.connect();
+        PreparedStatement psr;
+        String sql = null;
+        try {
+            sql = "insert into logfile (request, auth, ident, httpmethod, time, response, bytes, clientip, rawrequest, data, timestamp, paese) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?,?, ?)";
+            psr = c.prepareStatement(sql);
+            psr.setString(1, request);
+            psr.setString(2, auth);
+            psr.setString(3, ident);
+            psr.setString(4, httpmethod);
+            psr.setString(5, time);
+            psr.setInt(6, response);
+            psr.setInt(7, bytes);
+            psr.setString(8, clientip);
+            psr.setString(9, rawrequest);
+            psr.setString(10, data);
+            psr.setString(11, timestamp);
+            psr.setString(12, paese);
+            psr.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }finally {
+            try {
+                c.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ code/TestLetturaLog/src/db/Dblog.java
@@ -0,0 +1,125 @@
+package db;
+
+import java.io.File;
+import java.sql.*;
+
+import static java.sql.DriverManager.getConnection;
+
+public class Dblog {
+    private final String DBNAME;
+
+    public Dblog(String dbname) {
+        this.DBNAME = dbname;
+    }
+
+   /* public boolean signup(String nome, String cognome, String email, String password, String professione) throws SQLException {
+        DbSignup signup = new DbSignup();
+        return signup.signup(nome, cognome, email, password, professione, this);
+    }*/
+
+    /**
+     * Controlla di aver creato il Database e la tabella degli utenti
+     */
+    public void checkCreateDb() {
+        File file = new File(DBNAME);
+        if (file.exists()) {
+            System.out.println("Il database esiste");
+        } else {
+            try {
+                // ottengo il percorso per il database
+                //String percorso = this.getPercorso();
+                Class.forName("org.sqlite.JDBC");
+                Connection c = getConnection(
+                        "jdbc:sqlite:" + DBNAME);
+                //Connection c = getConnection("jdbc:sqlite:"+File.separator+DBNAME);
+                // se non esiste, creo la tabella degli utenti
+                createTableUser(c);
+            } catch (Exception e) { // eventuali errori
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
+            }
+        }
+    }
+
+    /**
+     * Si connette al Db
+     *
+     * @return Connection, la connessione al Db
+     */
+    public Connection connect() {
+        Connection c = null;
+        try {
+            // ottiene il percorso al db
+            //String percorso = this.getPercorso();
+            Class.forName("org.sqlite.JDBC");
+            //c = getConnection("jdbc:sqlite:" + percorso + File.separator + "database" + File.separator + DBNAME);
+            c = getConnection("jdbc:sqlite:" + DBNAME);
+        } catch (Exception e) {
+            // eventuali errori
+            System.exit(0);
+        }
+        return c;
+    }
+
+    /**
+     * Crea la tabella utenti del db
+     *
+     * @param c
+     */
+    private void createTableUser(Connection c) {
+        try {
+            Statement stmt = c.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS logfile " +
+                    "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "request TEXT NOT NULL, " +
+                    "auth TEXT NOT NULL," +
+                    "ident TEXT NOT NULL," +
+                    "httpmethod TEXT NOT NULL," +
+                    "time TEXT NOT NULL," +
+                    "response INT NOT NULL," +
+                    "bytes INT NOT NULL, " +
+                    "clientip TEXT NOT NULL," +
+                    "rawrequest TEXT NOT NULL," +
+                    "data TEXT NOT NULL, " +
+                    "timestamp TEXT NOT NULL, " +
+                    "paese TEXT NOT NULL)";
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Serve per ottenere il percorso assoluto della root del progetto
+     *
+     * @return
+     */
+    public String getPercorso() {
+        File f = new File("");
+        String percorso = f.getAbsolutePath();
+        if (percorso.contains("/code/backend")) {
+            percorso = percorso.replace("/code/backend", "/");
+        } else if (percorso.contains("/code")) {
+            percorso = percorso.replace("/code", "/");
+        }
+        return percorso;
+    }
+
+    /**
+     * ritorna l'ID dell'utente se il login è andato a buon fine, sennò ritorna 0
+     */
+    /*public int login(String email, String password) throws SQLException {
+        DbLogin login = new DbLogin();
+        return login.login(email, password, this);
+    }
+    */
+    public boolean insert(String request, String auth, String ident, String httpmethod, String time, int response,
+                          int bytes, String clientip, String rawrequest, String data, String timestamp, String paese) throws SQLException {
+        DbInsert dbInsert = new DbInsert();
+        return dbInsert.insert(request, auth, ident, httpmethod, time, response, bytes, clientip, rawrequest, data, timestamp, paese, this);
+    }
+}
+
