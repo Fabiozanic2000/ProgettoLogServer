78de905cc9808664965f66f07e491cf002676470
Antonio Benevento <271301@studenti.unimore.it>
2022-05-09 11:18:23 +0200

    Modificatato Mappa.js

--- code/frontend/src/js/Mappa.js
+++ code/frontend/src/js/Mappa.js
@@ -1,30 +1,24 @@
-
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
 import '../css/Mappa.css';
 
 const Mappa = () => {
-
-    
-
-
-
 
     return (
 
         <div className="mappa">
             <MapContainer center={[11.505, -0.09]} zoom={1} scrollWheelZoom={false}>
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
 
-                <Marker position={[51.505, -0.09] /* puntatore inghilterra */}>
+                <Marker position={[51.505, -0.09] /* puntatore inghilterra /}>
                     <Popup class="buono">
                         Inghilterra
                     </Popup>
                 </Marker>
 
-                <Marker position={[60.505, 55.09] /* puntatore russia */}>
+                <Marker position={[60.505, 55.09] / puntatore russia */}>
                     <Popup>
                         Russia
                     </Popup>
