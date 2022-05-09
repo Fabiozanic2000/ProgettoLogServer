935002f6c333a0911047b7c7826bf6ab10296abb
Antonio Benevento <271301@studenti.unimore.it>
2022-05-09 11:17:44 +0200

    Modificatato Login.js

--- code/frontend/src/js/Login.js
+++ code/frontend/src/js/Login.js
@@ -1,17 +1,17 @@
-import { sha3_512 } from 'js-sha3';
-import { useRef } from 'react';
 import '../css/Form.css';
 import '../css/Sfondo.css';
+import { sha3_512 } from 'js-sha3';
+import { useRef } from 'react';
 import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const Login = () => {
+
     //servono per inviare i dati nel form
     const emailInput = useRef();
     const passwordInput = useRef();
 
-    const handleLoginForm = async (e) =>
-    {
+    const handleLoginForm = async (e) => {
         e.preventDefault(); //evita di ricaricare la pagina
         const url = "http://localhost:9000/login"; //url al server java
 
@@ -20,22 +20,17 @@ const Login = () => {
         password = sha3_512(password); //cifro la password
 
         const corpo = {email: email, password: password, withCredentials: true}; //creo l'oggetto json da inviare al server
-        
+
         //INVIO I DATI
         const risposta = await axios.post(url, corpo);
 
         //guardo cosa è successo
-        if (risposta.data.id)
-        {
+        if (risposta.data.id) {
             alert("Autenticazione avvenuta con successo");
             window.location.href = window.location.href + "home";
         }
         else
-        {
             alert(risposta.data.errore);
-        }
-        
-        
     }
 
     return (
@@ -43,16 +38,12 @@ const Login = () => {
             <h1>Login</h1>
 
             <form onSubmit = {handleLoginForm}>
-            { /* <form> */ }
                 <input className='testo' type='text' ref={emailInput} id='email' name='email' placeholder='Email' required /> <br /> <br />
                 <input className='testo' type='password' ref={passwordInput} id='password' name='password' placeholder='Password' required /> <br /> <br />
                 <button id='bottone' type='submit'> Invia</button>
             </form>
         </div> 
-        
-
-
-     );
+    );
 }
  
 export default Login;
