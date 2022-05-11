b3adceb428062ac297aa2a0b71771c0696b3a288
superflem <alex.flem@hotmail.it>
2022-05-11 17:56:31 +0200

    migliorate alcune cose nel loggato Co-authored-by: Luca Dall'Olio <275185@studenti.unimore.it>

--- code/frontend/src/js/Loggato.js
+++ code/frontend/src/js/Loggato.js
@@ -7,31 +7,19 @@ import Elimina from './Elimina';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; //servono per andare in diverse pagine
 import {useEffect} from 'react';
 import {useState} from 'react';
-import axios from 'axios';
 import datiGrafici from '../funzioni/datiGrafici';
+import defaults from '../funzioni/defaultGrafici';
+import axios from 'axios';
 axios.defaults.withCredentials = true;
+
 
 const Loggato = () => {
 
-    //costruisco il grafico delle comunicazioni di default
-    const [datiGraficoComunicazioni, setDatiGraficoComunicazioni] = useState({
-        labels: ["Deafult"], //lista delle labels della torta
-        datasets: [{
-            label: "Comunicazioni",
-            data: [1], //dati che usa per creare il grafico
-            backgroundColor: ["black"] //colora le barre
-        }],
-    });
+   //costruisco il grafico delle comunicazioni di default
+   const [datiGraficoComunicazioni, setDatiGraficoComunicazioni] = useState(defaults.comunicazioniDefault);
 
     //costruisco il grafico dei posti di default
-    const [datiGraficoPosti, setDatiGraficoPosti] = useState({
-        labels: ["Deafult"], //lista delle labels della torta
-        datasets: [{
-            label: "Posti",
-            data: [1], //dati che usa per creare il grafico
-            backgroundColor: ["black"] //colora le barre
-        }],
-    });
+    const [datiGraficoPosti, setDatiGraficoPosti] = useState(defaults.postiDefault);
 
     const [nome, setNome] = useState('');
     const [professione, setProfessione] = useState('');    
@@ -59,8 +47,8 @@ const Loggato = () => {
         //controllo che i campi siano diversi da null
         if (testo === null) testo = "";
         if (stato === null) stato = "";
-        if (from === null) from = Math.round(new Date().getTime() / 1000);
-        if (to === null) to = Math.round(new Date().getTime() / 1000);
+        if (from === null) from = Math.round(new Date().getTime() / 1000) - 90000;
+        if (to === null) to = Math.round(new Date().getTime() / 1000) + 90000;
         if (scegli === null) scegli = "";
 
         const url2 = "http://localhost:9000/query";
