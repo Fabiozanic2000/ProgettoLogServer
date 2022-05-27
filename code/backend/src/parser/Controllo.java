28c7c2d16186c13fade715a4aa935c30e9585526
Il-castor <274987@studenti.unimore.it>
2022-05-27 07:50:20 +0200

    Sistemati alcuni bug e aggiornata documentazione

--- code/backend/src/parser/Controllo.java
+++ code/backend/src/parser/Controllo.java
@@ -7,10 +7,20 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe che permette di controllare se un indirizzo ip è sospetto
+ */
 public class Controllo {
     private int contatore = 0;
     private long lastTimestamp = -1;
 
+    /**
+     * Metodo per controllare più di 4 volte in poco tempo un ip sospetto
+     * @param currentTime timestamp corrente
+     * @param threshold soglia minima per vedere se un ip è sospetto
+     * @param ip_address indirizzo ip sospetto
+     * @param stato lo Stato dell'indirizzo ip
+     */
     public void check(long currentTime, int threshold, String ip_address, String stato) {
         if (lastTimestamp == -1) {
             lastTimestamp = currentTime;
